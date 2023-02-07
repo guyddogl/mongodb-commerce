837a3997ffdf220d636d81020d403d5fdc21c230
Guyddo <guyddogl@gmail.com>
2023-02-07 19:08:17 -0300

    Correções de lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,6 +1,6 @@
 db.produtos
   .find({
     valoresNutricionais: {
-      $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 }, }
-    }
+      $elemMatch: { tipo: "proteínas", percentual: { $lte: 40, $gte: 30 } },
+    },
   }, { _id: 0, nome: 1 });
